6e32de11d4aefd0ec8755a9b3f76b26efc06f18e
Huy Nguyen <huy.nguyentrong2000@gmail.com>
2022-11-02 22:49:44 +0700

    Add love folder and template folder for testing

---
+++ template/main.lua
@@ -0,0 +1,20 @@
+-- For more information, please visit:
+-- https://love2d-community.github.io/love-api/
+
+
+function love.load()
+  -- Everything will be loaded here
+
+end
+
+
+function love.update(dt)
+  -- Everything will be updated here
+
+end
+
+
+function love.draw()
+  -- Everything will be drawn here
+  
+end
